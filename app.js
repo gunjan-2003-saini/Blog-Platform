ea64e8ff824bbbd87516b2a16b75bb5008573649
gunjan <gunjan020303@gmail.com>
2025-02-08 20:05:06 +0530

    message

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
-{
-  "name": "blog-platform",
-  "version": "1.0.0",
-  "description": "",
-  "main": "app.js",
-  "scripts": {
-    "start": "node app.js",
-    "dev": "npx nodemon app.js"
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "bcryptjs": "^2.4.3",
-    "cors": "^2.8.5",
-    "dotenv": "^16.0.3",
-    "express": "^4.18.2",
-    "jsonwebtoken": "^9.0.0",
-    "mongoose": "^7.0.3"
-  },
-  "devDependencies": {
-    "nodemon": "^2.0.22"
-  }
-}
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(express.static('public'));
+
+// Routes
+const authRoutes = require('./routes/auth');
+const postRoutes = require('./routes/posts');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
